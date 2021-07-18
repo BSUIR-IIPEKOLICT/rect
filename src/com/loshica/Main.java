9d66f8ba00ebcbd698f80fae66e0147320ef5dc4
Zhenek <54858818+IIPEKOLICT@users.noreply.github.com>
2021-07-18 14:07:09 +0300

    add solution

---
+++ src/com/loshica/Main.java
@@ -0,0 +1,31 @@
+package com.loshica;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Программа для работы с прямоугольниками");
+
+        Scanner in = new Scanner(System.in);
+        Rect rect1 = Rect.create("\nСоздание 1 прямоугольника\n", in);
+
+        System.out.println("Периметр 1 прямоугольника: " + Rect.per(rect1));
+        System.out.println("Площадь 1 прямоугольника: " + Rect.area(rect1));
+
+        Rect rect2 = Rect.create("\nСоздание 2 прямоугольника\n", in);
+
+        if (Rect.contains(rect1, rect2)) {
+            System.out.println("1 прямоугольник содержится во 2.");
+        } else {
+            System.out.println("1 прямоугольник не содержится во 2.");
+        }
+
+        Rect rect3 = new Rect(356);
+        System.out.println("\nДлина 3 прямоугольника: " + Rect.getW(rect3));
+        System.out.println("Ширина 3 прямоугольника: " + Rect.getH(rect3));
+
+        Rect rect4 = new Rect();
+        System.out.println("\nДлина 4 прямоугольника: " + Rect.getW(rect4));
+        System.out.println("Ширина 4 прямоугольника: " + Rect.getH(rect4));
+    }
+}
---
+++ src/com/loshica/Rect.java
@@ -0,0 +1,49 @@
+package com.loshica;
+import java.util.Scanner;
+
+public class Rect {
+
+    private float w = 10;
+    private float h = 10;
+
+    public Rect(float w, float h) {
+        this.w = w;
+        this.h = h;
+    }
+
+    public Rect(float w) {
+        this.w = w;
+    }
+
+    public Rect() {}
+
+    public static Rect create(String text, Scanner in) {
+        System.out.println(text);
+        System.out.print("Введите длину прямоугольника: ");
+        float w = in.nextFloat();
+        System.out.print("Введите высоту прямоугольника: ");
+        float h = in.nextFloat();
+
+        return new Rect(w, h);
+    }
+
+    public static float per(Rect rect) {
+        return 2 * rect.w + 2 * rect.h;
+    }
+
+    public static float area(Rect rect) {
+        return rect.w * rect.h;
+    }
+
+    public static boolean contains(Rect rect1, Rect rect2) {
+        return rect1.w <= rect2.w && rect1.h <= rect2.h;
+    }
+
+    public static float getW(Rect rect) {
+        return rect.w;
+    }
+
+    public static float getH(Rect rect) {
+        return rect.h;
+    }
+}
